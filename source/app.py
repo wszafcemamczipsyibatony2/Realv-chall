cabd40b3b4e014aa2a8131ac52ddd8f77707f5b4
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2022-11-26 19:59:21 +0100

    xx

--- source/app.py
+++ source/app.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template, request
+import os
+
 app = Flask(__name__)
-flag = open('flag.txt').read()
-db = [i.strip() for i in open('database.db', 'r').readlines()]
+flag = open("flag.txt").read()
+db = [i.strip() for i in open("database.db", "r").readlines()]
 db.append(flag)
+TEAM_LINK = os.getenv("TEAM_LINK")
+
 
 def check_if_in(input):
-	for record in db:
-		if input in record:
-			return True
-	return False
+    for record in db:
+        if input in record:
+            return True
+    return False
 
-@app.route('/')
+
+@app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template("index.html", TEAM_LINK=TEAM_LINK)
 
-@app.route('/submit', methods=['POST'])
+
+@app.route("/submit", methods=["POST"])
 def check():
-	req = request.data.decode('utf-8')
-	return {'error': not check_if_in(req)}
+    req = request.data.decode("utf-8")
+    return {"error": not check_if_in(req)}
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
